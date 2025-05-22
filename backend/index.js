8c48befb39ca0c43c7d97779eed61b36cb0fbb19
Tejas-987 <wangulkartejas@gmail.com>
2025-05-22 15:25:06 +0530

    Initial commit

---
+++ backend/index.js
@@ -0,0 +1,27 @@
+require("dotenv").config();
+
+const express = require("express");
+const cors = require("cors");
+const geminiRoute = require("./src/routes/geminiRoute");
+const articleRoute = require("./src/routes/articleRoute");
+const app = express();
+app.head("/ping", (req, res) => {
+  res.status(200).end();  
+});
+app.use(
+  cors({
+    origin: ["http://localhost:5173"],
+  })
+);
+app.use(express.json());
+app.use("/ai", geminiRoute);
+app.use("/articles", articleRoute);
+const PORT = process.env.PORT || 8080;
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).json({ error: "Something went wrong!" });
+});
+
+app.listen(PORT, () => {
+  console.log("Listening successfully");
+});
